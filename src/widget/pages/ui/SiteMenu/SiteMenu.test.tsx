a4de55950f577f5544bffff796f06232d2de3c7e
VanyaMate <buhu653@mail.ru>
2024-03-12 19:08:07 +0300

    delete e2e from vitest

--- src/widget/pages/ui/SiteMenu/SiteMenu.test.tsx
+++ src/widget/pages/ui/SiteMenu/SiteMenu.test.tsx
@@ -1,13 +1,7 @@
 import { describe, it } from 'vitest';
 import SiteMenuRenderIt from '@/widget/pages/ui/SiteMenu/_tests_/SiteMenu.render.it.tsx';
-import SiteMenuRenderWithDesktopSizeIt
-    from '@/widget/pages/ui/SiteMenu/_e2e_/SiteMenu.render-with-desktop-size.e2e-test.tsx';
-import SiteMenuRenderWithMobileSizeIt
-    from '@/widget/pages/ui/SiteMenu/_e2e_/SiteMenu.render-with-mobile-size.e2e-test.tsx';
 
 
 describe('SiteMenu', () => {
     it('Render', SiteMenuRenderIt);
-    it('Render on desktop size (1300 width)', SiteMenuRenderWithDesktopSizeIt);
-    it('Render on mobile size (320 width)', SiteMenuRenderWithMobileSizeIt);
 });
