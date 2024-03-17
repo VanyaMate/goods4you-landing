595bcd31a1f56a4e31882fcb98131b364dd46a5c
VanyaMate <buhu653@mail.ru>
2024-03-17 03:23:33 +0300

    add test on Button | %pw_update%

---
+++ src/shared/ui/buttons/Button/_e2e_/Button.all-on-page.e2e-test.tsx
@@ -0,0 +1,33 @@
+import { expect, test } from '@playwright/experimental-ct-react';
+import { delay } from '$/_tests_/vitest/helpers/delay.ts';
+import Button from '@/shared/ui/buttons/Button/Button.tsx';
+
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('Menu with burger on mobile size', async ({ mount, page }) => {
+    // Mount a component. Returns locator pointing to the component.
+    const component = await mount(
+        <div style={ { display: 'flex', flexDirection: 'column', gap: 40 } }>
+            <Button>Empty</Button>
+            <Button quad>Q</Button>
+            <Button className="button_clear">Clear</Button>
+            <Button styleType="primary">Primary</Button>
+            <Button styleType="second">Second</Button>
+            <Button styleType="light">Light</Button>
+            <Button styleType="border">Border</Button>
+        </div>,
+    );
+
+    await expect(component).toContainText('Empty');
+    await expect(component).toContainText('Q');
+    await expect(component).toContainText('Clear');
+    await expect(component).toContainText('Primary');
+    await expect(component).toContainText('Second');
+    await expect(component).toContainText('Light');
+    await expect(component).toContainText('Border');
+
+    await delay(100);
+
+    await expect(page).toHaveScreenshot();
+});
